d9928a844a812d39505a59b0beb25b982a31a862
Ashish Ranjan <ash.ranjan09@gmail.com>
2026-02-19 18:26:29 +0530

    structureAndSemantics topics completed

--- src/App.jsx
+++ src/App.jsx
@@ -35,7 +35,7 @@ const App = () => {
                     {/* <div className="aboutHtml"></div> */}
                     <AboutHtml />
 
-                    <TopicsMap />
+                    {/* <TopicsMap /> */}
 
                     {/* foundation */}
                     <Introduction />
